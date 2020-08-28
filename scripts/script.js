2f7afa02f88a45eca482ab40b3e17cc7f815ce4e
spencermdavis <spencermdavis@mac.com>
2020-08-28 12:01:21 -0400

    fixed height issue added mobile nav menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 const portfolio = {};
 
-portfolio.init = () => {};
+// listen for user click on menu button
+portfolio.popoutSummon = () => {
+  $(".menuButton").on("click", function () {
+    // reveal sidebar
+    $(".sideList").toggleClass("sideListOpen");
+    $(".menuButton").toggleClass("menuButtonOpen");
+    $(".navList").toggleClass("divHider");
+  });
+};
+
+portfolio.init = () => {
+  portfolio.popoutSummon();
+};
 
 $(function () {
   portfolio.init();
